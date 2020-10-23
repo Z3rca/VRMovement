6c782aefd312e9a00aa6fd0b11807ed113d1b7c3
Linus <linus-tiemann@live.de>
2020-10-23 17:05:50 +0100

    added git ignore and basic scene

---
+++ Assets/Movement/NavMeshMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class NavMeshMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            agent.Move(Vector3.forward);;
+        }
+    }
+
+
+
+    public void MoveDirection(Vector3 direction)
+    {
+        agent.Move(Time.deltaTime*speed*direction);
+    }
+    
+}
