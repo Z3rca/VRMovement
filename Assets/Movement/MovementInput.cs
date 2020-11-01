7d530e6560361c101f2708b22d3ae5db29f6aaf2
Linus <linus-tiemann@live.de>
2020-11-01 14:19:16 +0100

    improved Input System
    The Input now resides on a seperate script, hands are visible again. Movement is now a method and not called in update loop of Mover.

---
+++ Assets/Movement/MovementInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+using Valve.VR;
+
+public class MovementInput : MonoBehaviour
+{
+    public bool DisableSideWayMovement;
+    
+    public SteamVR_ActionSet actionSetEnable;
+    public SteamVR_Action_Vector2 Input;
+
+    private NavMeshMovement Mover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actionSetEnable.Activate();
+        Mover = GetComponent<NavMeshMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 Movement = Input.GetAxis(SteamVR_Input_Sources.Any);
+        
+        if (DisableSideWayMovement)
+        {
+            Movement.x = 0;
+        }
+        
+        Mover.Move(Movement);
+        
+       
+
+        
+    }
+}
--- Assets/Movement/NavMeshMovement.cs
+++ Assets/Movement/NavMeshMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,33 +9,30 @@ using Valve.VR;
 public class NavMeshMovement : MonoBehaviour
 {
     [SerializeField] private Transform  _hmdTransform;
-    private Rigidbody _rigidBody;
     [SerializeField] private GameObject  head;
-    private SteamVR_Input_Sources hand;
     private NavMeshAgent agent;
-    public SteamVR_Action_Vector2 moveAction = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("platformer", "Move");
     public float speed;
     public bool DisableSideWayMovement;    
-    private Vector3 MovementDirection;
+    private Vector3 BodyDirection;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        _rigidBody = GetComponent<Rigidbody>();
         //agent.updateRotation = true;
         
+    }
+
+    private void FixedUpdate()
+    {
+        head.transform.position = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //agent.updatePosition = false;
-        //agent.updateRotation = false;
         
         Quaternion targetRot = Quaternion.LookRotation(_hmdTransform.forward);
         Vector3 eulerRotation = new Vector3();
-
-       
         
         eulerRotation= Vector3.ProjectOnPlane(targetRot.eulerAngles, Vector3.forward);
         eulerRotation.x = 0f;
@@ -44,55 +42,26 @@ public class NavMeshMovement : MonoBehaviour
         this.transform.rotation = targetRot;
         
         
-        
-        Vector2 m = moveAction[hand].axis;
-
-
-        if (DisableSideWayMovement)
-        {
-            m.x = 0;
-        }
-        
-        // rotate towards a vector
-     
-        //Debug.Log("input" + m);
         agent.angularSpeed = 300;
-        //agent.updateRotation = false;
+        
+    }
 
+    public void Move(Vector2 direction)
+    {
+        BodyDirection = new Vector3( direction.x,0f,direction.y  );
+        BodyDirection =  this.transform.rotation* BodyDirection;
+        Debug.Log("body" + BodyDirection);
+        Debug.Log("head" + direction);
+        MoveWith(BodyDirection);
+    }
 
-
+    private void MoveWith(Vector3 direction)
+    {
         
-        MovementDirection = new Vector3( m.x,0f,m.y  );
-            
-        MovementDirection =  transform.rotation* MovementDirection;
-        //MovementDirection = MovementDirection * 90; 
-
-        //MovementDirection = MovementDirection + eulerRotation;
-        
-        //Debug.Log("movement" + MovementDirection);
-        
-        //Debug.Log("this rotation"+ this.transform.rotation);
-        {
-        }
-        
-        
-        
-        
-       // Debug.Log("movement" + MovementDirection);
-        
-        
-        Move(MovementDirection);
-        
-        
+        agent.Move(Time.deltaTime*speed*direction);
        
     }
-
-
-
-    public void Move(Vector3 direction)
-    {
-        agent.Move(Time.deltaTime*speed*direction);
-        head.transform.position = this.transform.position;
-    }
+    
+    
     
 }
